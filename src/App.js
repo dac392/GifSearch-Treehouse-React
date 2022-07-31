5183912ed27bcb8983947f2754679bd524b7a7d3
dac392 <dac392@scarletmail.rutgers.edu>
2022-07-31 16:29:04 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -9,24 +9,19 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-        gifs: []
+        gifs: [],
+        loading: true
     };
   } 
 
   componentDidMount(){
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=6r6ZPCbdpEuVFo3gw8rOF8ccLS9I6CPt')
-        .then(response => {
-            this.setState({ gifs: response.data.data });
-        })
-        .catch(error => {
-            console.log('Error fetching and parsing data', error);
-        });
+    this.performSearch();
   }
 
-  performSearch = (query) =>{
+  performSearch = (query="anime") =>{
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=6r6ZPCbdpEuVFo3gw8rOF8ccLS9I6CPt`)
         .then(response => {
-            this.setState({ gifs: response.data.data });
+            this.setState({ gifs: response.data.data, loading: false });
         })
         .catch(error => {
             console.log('Error fetching and parsing data', error);
@@ -44,7 +39,11 @@ export default class App extends Component {
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+            {
+                (this.state.loading)
+                ? <p>Loading...</p>
+                : <GifList data={this.state.gifs} />
+            }
         </div>
       </div>
     );
