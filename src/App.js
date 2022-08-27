e7a18d7e9c94fc60038f9fa6fe1290293f7b9e78
dac392 <dac392@scarletmail.rutgers.edu>
2022-08-27 13:44:56 -0400

    converted App.js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import axios from 'axios';
-import SearchForm from './Components/SearchForm';
+
+import SearchForm from './Components//SearchForm';
 import GifList from './Components/GifList';
 
-export default class App extends Component {
-  
-  constructor() {
-    super();
-    this.state = {
-        gifs: [],
-        loading: true
-    };
-  } 
+function App() {
+  const [ data, setData ] = useState([]);
+  const [ query, setQuery ] = useState('cats');
+  const performSearch = ( value )=> setQuery(value);
+  useEffect( () => {
+    axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=6r6ZPCbdpEuVFo3gw8rOF8ccLS9I6CPt`)
+      .then(response => setData(response.data.data))
+      .catch(error => console.log({ message: 'Error fetching and parsing data', error: error.message }));
+  }, [ query ]);
 
-  componentDidMount(){
-    this.performSearch();
-  }
-
-  performSearch = (query="anime") =>{
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=6r6ZPCbdpEuVFo3gw8rOF8ccLS9I6CPt`)
-        .then(response => {
-            this.setState({ gifs: response.data.data, loading: false });
-        })
-        .catch(error => {
-            console.log('Error fetching and parsing data', error);
-        });
-  }
-
-  render() { 
-    console.log(this.state.gifs);
-    return (
-      <div>
-        <div className="main-header">
-          <div className="inner">
-            <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch} />      
-          </div>   
-        </div>    
-        <div className="main-content">
-            {
-                (this.state.loading)
-                ? <p>Loading...</p>
-                : <GifList data={this.state.gifs} />
-            }
+  return (
+    <>
+      <div className="main-header">
+        <div className="inner">
+          <h1 className="main-title">GifSearch</h1>
+          <SearchForm onSearch={performSearch} />
         </div>
       </div>
-    );
-  }
+      <div className="main-content">
+        <GifList data={data} />
+      </div>
+    </>
+  );
 }
+
+export default App
+
